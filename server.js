7ad523b92f8ab9def00c81ed0826ed6b69ce0c00
NosyApp <ivan.blanco.martinez@gmail.com>
2017-01-17 13:56:22 +0100

    Created a new page and his partials

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-// Take the heroku var or if not the port 3000
 const port = process.env.PORT || 3000;
-
 var app = express();
-//To use partials parts of hbs.
-hbs.registerPartials(__dirname + '/views/partials');
-//Necessary to make express known about the view engine that we want to use.
+
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-//app.use is the way to register a midleware
-//next: exist to tell express when your midleware function is done.
 app.use(function (req, res, next) {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
@@ -25,17 +20,12 @@ app.use(function (req, res, next) {
   next();
 });
 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
-//Defining a render for maintenance.hbs
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
-
-//Midleware to set static directories.
-//__direname -> give you the complete path
 app.use(express.static(__dirname + '/public'));
 
-//Hbs helper for date
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -44,44 +34,32 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-//Getter, with url as first parameter and function of second parameter
-//Function will have a request and response as parameters.
-// Localhost:3000
 app.get('/', (req, res) => {
-
- res.render('home.hbs', {
-   pageTitle: 'Home Page',
-   messagePage: 'Lorem ipsum'
- });
-
-  //Respond of a html file.
-  // res.send('<h1>Hello express</h1>');
-
-  // //Respond of a Json file.
-  // res.send({
-  //   name: 'Ivan',
-  //   likes: [
-  //     'MMA',
-  //     'Traveling'
-  //   ]
-  // });
-});
-
-// localhost:3000/about --> we will get this respond.
-app.get('/about', function(req, res) {
-  //The second parameter is the object with the dynamic variables to use in the hbs file.
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-    });
-});
-
-// /bad -> send back json with errorMessage
-app.get('/bad', function(req, res) {
-  res.send({
-    errorMessage : 'Unable to handle request'
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.get('/portfolio', function(req, res) {
+  res.render('portfolio.hbs', {
+    pageTitle: 'Portfolio Page',
+    porftolioMessage: 'Portfolio Page here'
+    });
+})
 //app listen in port of heroku
 app.listen(port, function() {
   console.log('Server is up on port 3000');
